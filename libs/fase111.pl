894dfa784ed82d58aa5fcf39f6d78da1543e97ea
Marcos <marcositu@gmail.com>
2014-08-01 15:10:14 -0300

    cambios fase 1.11

--- libs/fase111.pl
+++ libs/fase111.pl
@@ -25,20 +25,16 @@ while (<ID0PASSWD>) {
        $user=$userdata[0];
        $id0=$userdata[3];
 	   
-	   if ($user ne "root")
+	   if ($username ne "root" && $username ne "halt" && $username ne "sync" && $username ne "shutdown" && $username ne "operator")
 	   {
 			if ($id0 == 0) {
-			 if ($username ne "root" && $username ne "halt" && $username ne "sync" && $username ne "shutdown" && $username ne "operator")
-			 {
-			 
+		 
 				print BOLD, RED, "\nNegativo:", RESET . " Negativo: El usuario $user  tiene definido Grupo ID 0\n";
 				print F "<p class=\"negativo\">El usuario $user  tiene definido Grupo ID 0</p>\n";
 				print R "<p>El usuario $user  tiene definido Grupo ID 0. Recomendamos analizar si los permisos asignados son los correctos</p>\n";
 			}
 		}
 		}
-}
-
 
 print F "</div>\n";
 print F "</div>\n";
